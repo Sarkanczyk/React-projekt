bce8274232aefc63232a352e40bf61d575759ee0
Sarkanczyk <sarkanczyk@gmail.com>
2020-01-17 23:14:39 +0100

    poprawiono wyszukiwarke

--- my-app/src/components/Catalog/ManuItems.js
+++ my-app/src/components/Catalog/ManuItems.js
@@ -1,34 +1,53 @@
 import React, { useContext } from "react";
 import ProductsService from "../services/products.service";
 import ManuContext from "./ManuContext";
+import NameContext from "./NameContext";
 
 const products = ProductsService.getProducts();
 
 const ManuItems = () => {
   const manu = useContext(ManuContext);
+  const name = useContext(NameContext);
   return (
     <div class="column-right">
       <div class="products">
         {products.map(e => {
           if (manu === "all") {
-            return (
-              <div class="product">
-                <img src={e.image} alt={e.name} />
-                <p class="price">{e.amount}</p>
-                <h3>{e.name}></h3>
-              </div>
-            );
-          } else if (
-            e.manufacture === manu ||
-            e.name.indexOf(`${manu}`) !== -1
-          ) {
-            return (
-              <div class="product">
-                <img src={e.image} alt={e.name} />
-                <p class="price">{e.amount}</p>
-                <h3>{e.name}></h3>
-              </div>
-            );
+            if (name === "") {
+              return (
+                <div class="product">
+                  <img src={e.image} alt={e.name} />
+                  <p class="price">{e.amount}</p>
+                  <h3>{e.name}></h3>
+                </div>
+              );
+            } else if (e.name.indexOf(`${name}`) !== -1) {
+              return (
+                <div class="product">
+                  <img src={e.image} alt={e.name} />
+                  <p class="price">{e.amount}</p>
+                  <h3>{e.name}></h3>
+                </div>
+              );
+            }
+          } else if (e.manufacture === manu) {
+            if (name === "") {
+              return (
+                <div class="product">
+                  <img src={e.image} alt={e.name} />
+                  <p class="price">{e.amount}</p>
+                  <h3>{e.name}></h3>
+                </div>
+              );
+            } else if (e.name.indexOf(`${name}`) !== -1) {
+              return (
+                <div class="product">
+                  <img src={e.image} alt={e.name} />
+                  <p class="price">{e.amount}</p>
+                  <h3>{e.name}></h3>
+                </div>
+              );
+            }
           }
         })}
       </div>
--- my-app/src/components/Catalog/Search.js
+++ my-app/src/components/Catalog/Search.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState, useEffect } from "react";
 import ManuContext from "./ManuContext";
+import NameContext from "./NameContext";
 import ManuItems from "./ManuItems";
 
 function Search() {
@@ -10,127 +11,133 @@ function Search() {
   const onClear = () => {
     inputRef.current.value = "";
     setUserManu("all");
+    setUserName("");
   };
   const [userManu, setUserManu] = useState("all");
+  const [userName, setUserName] = useState("");
 
   return (
     <ManuContext.Provider value={userManu}>
-      <div class="column-left">
-        <div class="filter">
-          <div class="filter-header">
-            <h4>Search</h4>
-            <span class="clear" onClick={onClear}>
-              Clear
-            </span>
-          </div>
-          <div>
-            <input
-              id="field"
-              type="text"
-              placeholder="search..."
-              ref={inputRef}
-              onInput={() => setUserManu(`${inputRef.current.value}`)}
-            />
-          </div>
-          <h4>Manufacturer</h4>
-          <div>
-            <div>
-              <input
-                type="radio"
-                name="manufacturere"
-                id="all"
-                value="all"
-                onClick={() => setUserManu("all")}
-                defaultChecked
-              />
-              <label for="all">All</label>
+      <NameContext.Provider value={userName}>
+        <div class="column-left">
+          <div class="filter">
+            <div class="filter-header">
+              <h4>Search</h4>
+              <span class="clear" onClick={onClear}>
+                Clear
+              </span>
             </div>
             <div>
               <input
-                type="radio"
-                name="manufacturere"
-                id="apple"
-                value="apple"
-                onClick={() => setUserManu("Apple")}
+                id="field"
+                type="text"
+                placeholder="search..."
+                ref={inputRef}
+                onInput={() => setUserName(`${inputRef.current.value}`)}
               />
-              <label for="apple">Apple</label>
             </div>
+            <h4>Manufacturer</h4>
             <div>
-              <input
-                type="radio"
-                name="manufacturere"
-                id="dell"
-                value="dell"
-                onClick={() => setUserManu("Dell")}
-              />
-              <label for="dell">Dell</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                name="manufacturere"
-                id="lenovo"
-                value="lenovo"
-                onClick={() => setUserManu("Lenovo")}
-              />
-              <label for="lenovo">Lenovo</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                name="manufacturere"
-                id="microsoft"
-                value="microsoft"
-                onClick={() => setUserManu("Microsoft")}
-              />
-              <label for="microsoft">Microsoft</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                name="manufacturere"
-                id="xiaomi"
-                value="xiaomi"
-                onClick={() => setUserManu("Xiaomi")}
-              />
-              <label for="xiaomi">Xiaomi</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                name="manufacturere"
-                id="intel"
-                value="intel"
-                onClick={() => setUserManu("Intel")}
-              />
-              <label for="intel">Intel</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                name="manufacturere"
-                id="asus"
-                value="asus"
-                onClick={() => setUserManu("Asus")}
-              />
-              <label for="asus">Asus</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                name="manufacturere"
-                id="samsung"
-                value="samsung"
-                onClick={() => setUserManu("Samsung")}
-              />
-              <label for="samsung">Samsung</label>
+              <div>
+                <input
+                  type="radio"
+                  name="manufacturere"
+                  id="all"
+                  value="all"
+                  onClick={() => setUserManu("all")}
+                  defaultChecked
+                />
+                <label for="all">All</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  name="manufacturere"
+                  id="apple"
+                  value="apple"
+                  onClick={() => setUserManu("Apple")}
+                />
+                <label for="apple">Apple</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  name="manufacturere"
+                  id="dell"
+                  value="dell"
+                  onClick={() => setUserManu("Dell")}
+                />
+                <label for="dell">Dell</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  name="manufacturere"
+                  id="lenovo"
+                  value="lenovo"
+                  onClick={() => setUserManu("Lenovo")}
+                />
+                <label for="lenovo">Lenovo</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  name="manufacturere"
+                  id="microsoft"
+                  value="microsoft"
+                  onClick={() => setUserManu("Microsoft")}
+                />
+                <label for="microsoft">Microsoft</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  name="manufacturere"
+                  id="xiaomi"
+                  value="xiaomi"
+                  onClick={() => setUserManu("Xiaomi")}
+                />
+                <label for="xiaomi">Xiaomi</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  name="manufacturere"
+                  id="intel"
+                  value="intel"
+                  onClick={() => setUserManu("Intel")}
+                />
+                <label for="intel">Intel</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  name="manufacturere"
+                  id="asus"
+                  value="asus"
+                  onClick={() => setUserManu("Asus")}
+                />
+                <label for="asus">Asus</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  name="manufacturere"
+                  id="samsung"
+                  value="samsung"
+                  onClick={() => setUserManu("Samsung")}
+                />
+                <label for="samsung">Samsung</label>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-
-      <ManuItems />
+        <ManuItems />
+      </NameContext.Provider>
     </ManuContext.Provider>
+    // {/* <NameContext.Provider value={userName}>
+    //   <ManuItems />
+    // </NameContext.Provider> */}
   );
 }
 
